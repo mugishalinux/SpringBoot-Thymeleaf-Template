35f27cf4534e819ffa2db5a8dc4583b66c6b8a38
mugishalinux <mugishapacifique1@gmail.com>
2022-08-17 23:35:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# spring-boot-thymeleaf-staradmin-demo
+
+Spring Boot with Thymeleaf and Star Admin 
+
+## Installation
+
+`https://github.com/mugishalinux/SpringBoot-Thymeleaf-Template.git`
+
+`cd WebAdminExample`
+
+`mvn clean install`
+
+`cd target`
+
+`java -jar WebAdminExample-0.0.1-SNAPSHOT.jar`
+
+### Screenshoot
+![Dashboard](img/dashboard.png "Dashboard")
---
+++ src/test/java/com/farhan/staradmin/SpringBootThymeleafStarAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.farhan.staradmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootThymeleafStarAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
